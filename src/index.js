ec4e2ca58c2032cb9bcdf2d6b7a05a84a96300b0
tushardongre1990 <tushardongre.1990@gmail.com>
2023-12-30 00:44:20 +0530

    42. Passing and Receiving Props

--- src/index.js
+++ src/index.js
@@ -70,12 +70,35 @@ const Menu = () => {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      />
     </main>
   );
 };
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 10}</span>
+      </div>
+    </div>
+  );
+}
 const Footer = () => {
   const hour = new Date().getHours();
   const openHour = 10;
@@ -95,16 +118,6 @@ const Footer = () => {
     </footer>
   );
 };
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="pizzza-img" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
